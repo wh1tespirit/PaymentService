4baec951448691c08da83759775248989dce9ebf
korpaxdev <korpaxdev@gmail.com>
2025-05-13 18:22:29 +0200

    fix: logger

--- api/routes/sample/controller.py
+++ api/routes/sample/controller.py
@@ -1,10 +1,15 @@
+import logging
+
 from api.routes.sample.schemas.req import CreateSampleRequest
 from api.utils.di import MongoTypeDI
+from common.logger import Loggers
 from common.mongo.models.sample import SampleModel
 
 
 async def get_samples(mongo: MongoTypeDI):
     samples = await mongo.samples.find_many({}, limit=100, use_cache=False)
+    logger = logging.getLogger(Loggers.API_FILE)
+    logger.info("getting samples")
     return samples
 
 
--- common/logger.py
+++ common/logger.py
@@ -79,12 +79,19 @@ def setup_file_logger(
 def get_json_formatter(service_name: ServiceNames):
     class JSONFormatter(logging.Formatter):
         def format(self, record):
+            message_str = record.getMessage()
+
+            try:
+                data = json.loads(message_str)
+            except json.JSONDecodeError:
+                data = {"message_str": message_str}
+
             log_record = {
                 "date": get_moscow_time(),
                 "project_name": settings.PROJECT_NAME,
                 "service_name": service_name.value,
                 "level": record.levelname,
-                "message": record.getMessage(),
+                "data": data,
             }
             if record.exc_info:
                 exc_type, exc_value, exc_traceback = record.exc_info
